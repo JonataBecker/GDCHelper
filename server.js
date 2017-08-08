4aa76e0373523e2b63492859585c452ce9ba4a8e
JonataBecker <jonatabecker@gmail.com>
2017-08-07 21:16:32 -0300

    Configuração Heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
 
 const path = require('path');
 
