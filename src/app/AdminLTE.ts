b139836fb1dcfecce5f7f08356fae6131903f502
JonataBecker <jonatabecker@gmail.com>
2017-09-23 08:15:18 -0300

    Ajuste no menu da aplicação

--- src/app/AdminLTE.ts
+++ src/app/AdminLTE.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AdminLTE {
@@ -6,16 +6,4 @@ export class AdminLTE {
     fix() {
         eval('($.AdminLTE.layout ? $.AdminLTE.layout.fix() : "")');
     }
-    pushMenu() {
-        eval('($.AdminLTE.pushMenu ? $.AdminLTE.pushMenu.activate() : "")');
-    }
-
-    menuExpand() {
-        eval('($.AdminLTE.pushMenu ? $.AdminLTE.pushMenu.expand() : "")');
-    }
-
-    menuCollapse() {
-        eval('($.AdminLTE.pushMenu ? $.AdminLTE.pushMenu.collapse() : "")');
-    }
-
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,5 @@ export class AppComponent  implements OnInit {
 
   ngOnInit() {
     this.adminLTE.fix();
-    this.adminLTE.pushMenu();
   }
 }
