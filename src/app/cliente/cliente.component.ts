ed8f5b834868959c5e07c4a4ab23d6319d6808a4
marinalaux <laux.marina@gmail.com>
2017-09-24 18:23:00 -0300

    Disponibilizados dados dos contatos na página do cliente

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { URLSearchParams } from '@angular/http';
 import { WebserviceService } from 'app/webservice.service';
 
 @Component({
@@ -10,13 +11,21 @@ import { WebserviceService } from 'app/webservice.service';
 export class ClienteComponent implements OnInit {
 
   public cliente;
+  public contatos;
 
   constructor(private webservice: WebserviceService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let idCliente;
     this.route.params.subscribe(params => {
+      idCliente = params['idCliente'];
       this.webservice.get('cliente/' + params['idCliente'], null).subscribe((res) => {
         this.cliente = res.json();
+      });
+      const data = new  URLSearchParams();
+      data.append('idCliente', idCliente);
+      this.webservice.get('contatos', data).subscribe((res) => {
+        this.contatos = res.json();
       });
     });
   }
